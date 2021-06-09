8b2cff1c2e1df25d5f7532faf88aab35d0c382d5
Yoga Permana <yoga@xcidic.com>
2021-06-09 14:44:26 +0700

    add intl messages utils

--- data/index.js
+++ data/index.js
@@ -3,6 +3,21 @@ export const Logo = {
   dark: logoDark
 };
 
+export const languageData = [
+  {
+    languageId: 'english',
+    locale: 'en',
+    name: 'English',
+    icon: 'us'
+  },
+  {
+    languageId: 'spanish',
+    locale: 'es',
+    name: 'Spanish',
+    icon: 'es'
+  }
+]
+
 export const NavLinksData = [
   {
     name: "Home",
