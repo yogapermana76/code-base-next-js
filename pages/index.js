c55eb8c8811345c23d8f0fa99d80506dd26c752a
dbsSensei <dimasbagussusilo@gmail.com>
2021-06-10 21:22:20 +0700

    Create slicing header

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
-import React from "react";
-import Layout from "@/components/layout";
-import Footer from "@/components/footer";
-import AboutTwo from "@/components/about-two";
-import ClientCarousel from "@/components/client-carousel";
-import RecentProject from "@/components/recent-project";
-import CallToActionTwo from "@/components/call-to-action-two";
-import CallToActionThree from "@/components/call-to-action-three";
-import AboutThree from "@/components/about-three";
-import VideoDesignGuide from "@/components/video-design-guide";
-import TeamOne from "@/components/team-one";
-import FaqFeature from "@/components/faq-feature";
-import TestimonialsOne from "@/components/testimonials-one";
-import ServiceTwo from "@/components/service-two";
-import HeaderOne from "@/components/header-one";
-import SliderOne from "@/components/slider-one";
+import React from 'react';
+import Layout from '@/components/layout';
+import Footer from '@/components/footer';
+import AboutTwo from '@/components/about-two';
+import ClientCarousel from '@/components/client-carousel';
+import RecentProject from '@/components/recent-project';
+import CallToActionTwo from '@/components/call-to-action-two';
+import CallToActionThree from '@/components/call-to-action-three';
+import AboutThree from '@/components/about-three';
+import VideoDesignGuide from '@/components/video-design-guide';
+import TeamOne from '@/components/team-one';
+import FaqFeature from '@/components/faq-feature';
+import TestimonialsOne from '@/components/testimonials-one';
+import ServiceTwo from '@/components/service-two';
+import HeaderOne from '@/components/header-one';
+import SliderOne from '@/components/slider-one';
+
+import Header from '../components/Header/Header';
 
 const Home = () => {
   return (
     <Layout PageTitle="Home Page">
-      <HeaderOne />
-      <SliderOne />
+      {/* <HeaderOne /> */}
+      <Header />
+      {/* <SliderOne /> */}
       <AboutTwo />
       <ServiceTwo />
       <TestimonialsOne />
