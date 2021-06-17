b248cac004035230999c2644870a491f323e9b7d
Yoga Permana <yoga@xcidic.com>
2021-06-17 15:14:14 +0700

    fix footer styling

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from 'react';
-import Link from 'next/link';
 import classes from './Footer.module.css';
 import { Col, Row } from 'react-bootstrap';
 import KemluLogo from '@/images/footer/kemlu-logo.png';
 import KemendagLogo from '@/images/footer/kemendag-logo.png';
 import IndonesiaWonderfull from '@/images/footer/indonesia-wonderfull.png';
-import ICHome from '@/images/footer/home-ic.svg';
 import ICPhone from '@/images/footer/phone-ic.svg';
 import ICMessage from '@/images/footer/message-ic.svg';
 
@@ -21,85 +19,24 @@ const Footer = () => {
                 <img src={KemendagLogo} className="mr-3" />
                 <img src={IndonesiaWonderfull} />
               </div>
-              <div>
-                <h1>KEMENTERIAN LUAR NEGERI REPUBLIK INDONESIA</h1>
-                <div className={classes['address-footer']}>
-                  <div className={classes['address-detail']}>
-                    <img src={ICHome} />
-                    <span>
-                      Jl. Taman Pejambon No. 6, Jakarta Pusat, DKI Jakarta 10110
-                      Indonesia
-                    </span>
-                  </div>
-                  <div className={classes['address-detail']}>
-                    <img src={ICPhone} />
-                    <span>(+62) 21 344 1508</span>
-                  </div>
-                  <div className={classes['address-detail']}>
-                    <img src={ICMessage} />
-                    <span>info@ina-lac.com</span>
-                  </div>
-                </div>
-              </div>
             </Col>
-            <Col lg={7} className={`${classes['footer-menus']} float-right`}>
-              <div className={`${classes['footer-widget']}`}>
-                <ul>
-                  <li>
-                    <Link href="#">
-                      <a>Home</a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <a>About</a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <a>Contact Us</a>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className={`${classes['footer-widget']}`}>
-                <ul>
-                  <li>
-                    <Link href="#">
-                      <a>Trade</a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <a>Investment</a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <a>Tourisme</a>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className={`${classes['footer-widget']}`}>
-                <ul>
-                  <li>
-                    <Link href="#">
-                      <a>Regional Cluster</a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <a>Publications</a>
-                    </Link>
-                  </li>
-                </ul>
+            <Col lg={7} className={`${classes.footerMenus} float-right`}>
+              <div className={classes.addressFooter}>
+                <h3>KEMENTERIAN LUAR NEGERI REPUBLIK INDONESIA</h3>
+                <div className={classes.addressDetail}>
+                  <img src={ICPhone} />
+                  <span>(+62) 21 344 1508</span>
+                </div>
+                <div className={classes.addressDetail}>
+                  <img src={ICMessage} />
+                  <span>info@ina-lac.com</span>
+                </div>
               </div>
             </Col>
           </Row>
         </div>
       </footer>
-      <div className={classes['footer-bottom']}>
+      <div className={classes.footerBottom}>
         © 2021 Kementerian Luar Negeri Republik Indonesia. All Right Reserved.
       </div>
     </Fragment>
