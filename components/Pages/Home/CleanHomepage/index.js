d546468e426166b7c610e244b35a315eb40e25be
XCIDIC <xcidic@static.125.dscga.com>
2021-06-18 13:26:51 +0700

    fix wonderful and clean section

--- components/Pages/Home/CleanHomepage/index.js
+++ components/Pages/Home/CleanHomepage/index.js
@@ -9,22 +9,22 @@ import Nature from '@/images/pages/home/clean/nature.png';
 import ReadMore from '@/images/pages/home/clean/readMore.png';
 
 const Clean = () => {
-  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+  const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 425 });
+  const isTablet = useMediaQuery({ minWidth: 426, maxWidth: 768 });
   return (
-    <Fragment>
-      {/* {isMobile ? null : <div className={classes['verticalLine']} />} */}
-      <div className={classes['verticalLine']} />
+    <div>
+      <section className={`${classes.clean} ${classes.section}`}>
+        <div className={classes.verticalLine} />
 
-      <section className="welcome-section sec-pad">
-        <div className="thm-container">
+        <div className={classes.containerModify}>
           {isMobile ? (
             <div>
               <div>
-                <h3 className={classes['titleSection']}>
+                <h3 className={classes.titleSection}>
                   Clean and clear
                   <br /> investment projects
                 </h3>
-                <p className={classes['descSection']}>
+                <p className={classes.descSection}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
                   vulputate nulla mauris, sed <br /> velit. Auctor morbi nulla
                   facilisis interdum est suspendisse tristique. Auctor amet quis
@@ -32,60 +32,121 @@ const Clean = () => {
                   enim.
                 </p>
               </div>
-              <div
-                className={`${classes['card']} row flex-nowrap overflow-auto`}>
-                <Col className={classes['cardCol']} lg={4}>
-                  <div className={classes['divImg']}>
-                    <div className={classes['divReadMore']}>
-                      <p className={classes['project']}>Project Name </p>
-                      <p className={classes['title']}>
+              <div className={`${classes.card} row flex-nowrap overflow-auto`}>
+                <Col className={classes.cardCol} lg={4}>
+                  <div className={classes.divImg}>
+                    <div className={classes.divReadMore}>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>
                         Development and <br /> Optimisation of Puspa Agro
                       </p>
-                      <p className={classes['project']}>Project Name </p>
-                      <p className={classes['title']}>Agriculture </p>
-                      <p className={classes['project']}>Project Name </p>
-                      <p className={classes['title']}>East Java </p>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>Agriculture </p>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>East Java </p>
                       <Row>
                         <div className="col-14">
                           <img
                             src={ReadMore}
-                            className={classes['arrowReadMore']}
+                            className={classes.arrowReadMore}
                           />
                         </div>
                         <div className="col-6">
-                          <p className={classes['readMoreLink']}>Read more</p>
+                          <p className={classes.readMoreLink}>Read more</p>
                         </div>
                       </Row>
                       <p></p>
                     </div>
 
-                    <img className={classes['imgReadMore']} src={Nature} />
+                    <img className={classes.imgReadMore} src={Nature} />
                   </div>
                 </Col>
-                <Col className={classes['cardCol']} lg={4}>
-                  <div className={classes['divImg']}>
-                    <img className={classes['img']} src={Warehouse} />
+                <Col className={classes.cardCol} lg={4}>
+                  <div className={classes.divImg}>
+                    <img className={classes.img} src={Warehouse} />
                   </div>
                 </Col>
-                <Col className={classes['cardCol']} lg={4}>
-                  <div className={classes['divImg']}>
-                    <img className={classes['img']} src={Building} />
+                <Col className={classes.cardCol} lg={4}>
+                  <div className={classes.divImg}>
+                    <img className={classes.img} src={Building} />
                   </div>
                 </Col>
               </div>
               <Link href={'/'}>
-                <span className={classes['spanSeeMore']}>
-                  <a className={classes['seeMore']}>See more projects</a>
+                <span className={classes.spanSeeMore}>
+                  <a className={classes.seeMore}>See more projects</a>
                 </span>
               </Link>
             </div>
-          ) : (
+          ) : isTablet ? (
             <div>
-              <div className="text-center">
-                <h3 className={classes['titleSection']}>
+              <div className={classes.textCenter}>
+                <h3 className={classes.titleSection}>
                   Clean and clear investment projects
                 </h3>
-                <p className={classes['descSection']}>
+                <p className={classes.descSection}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
+                  vulputate nulla mauris, sed <br /> velit. Auctor morbi nulla
+                  facilisis interdum est suspendisse tristique. Auctor amet quis
+                  lorem <br /> laoreet mattis. Sagittis faucibus amet molestie
+                  enim.
+                </p>
+              </div>
+              <div className={`${classes.card} row flex-nowrap overflow-auto`}>
+                <Col className={classes.cardCol} lg={4}>
+                  <div className={classes.divImg}>
+                    <div className={classes.divReadMore}>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>
+                        Development and <br /> Optimisation of Puspa Agro
+                      </p>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>Agriculture </p>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>East Java </p>
+                      <Row>
+                        <div className="col-14">
+                          <img
+                            src={ReadMore}
+                            className={classes.arrowReadMore}
+                          />
+                        </div>
+                        <div className="col-6">
+                          <p className={classes.readMoreLink}>Read more</p>
+                        </div>
+                      </Row>
+                      <p></p>
+                    </div>
+
+                    <img className={classes.imgReadMore} src={Nature} />
+                  </div>
+                </Col>
+                <Col className={classes.cardCol} lg={4}>
+                  <div className={classes.divImg}>
+                    <img className={classes.img} src={Warehouse} />
+                  </div>
+                </Col>
+                <Col className={classes.cardCol} lg={4}>
+                  <div className={classes.divImg}>
+                    <img className={classes.img} src={Building} />
+                  </div>
+                </Col>
+              </div>
+              <div className={classes.textCenter}>
+                <Link href={'/'}>
+                  <span className={classes.spanSeeMore}>
+                    <a className={classes.seeMore}>See more projects</a>
+                  </span>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <div className={classes.textCenter}>
+                <h3 className={classes.titleSection}>
+                  Clean and clear investment projects
+                </h3>
+                <p className={classes.descSection}>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
                   vulputate nulla mauris, sed <br /> velit. Auctor morbi nulla
                   facilisis interdum est suspendisse tristique. Auctor amet quis
@@ -95,25 +156,25 @@ const Clean = () => {
               </div>
               <Row>
                 <Col lg={4}>
-                  <div className={classes['divImg']}>
-                    <div className={classes['divReadMore']}>
-                      <p className={classes['project']}>Project Name </p>
-                      <p className={classes['title']}>
+                  <div className={classes.divImg}>
+                    <div className={classes.divReadMore}>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>
                         Development and <br /> Optimisation of Puspa Agro
                       </p>
-                      <p className={classes['project']}>Project Name </p>
-                      <p className={classes['title']}>Agriculture </p>
-                      <p className={classes['project']}>Project Name </p>
-                      <p className={classes['title']}>East Java </p>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>Agriculture </p>
+                      <p className={classes.project}>Project Name </p>
+                      <p className={classes.title}>East Java </p>
                       <Row>
                         <Col lg={2}>
                           <img
                             src={ReadMore}
-                            className={classes['arrowReadMore']}
+                            className={classes.arrowReadMore}
                           />
                         </Col>
                         <Col lg={10}>
-                          <p className={classes['readMoreLink']}>
+                          <p className={classes.readMoreLink}>
                             Read more about the project
                           </p>
                         </Col>
@@ -121,24 +182,24 @@ const Clean = () => {
                       <p></p>
                     </div>
 
-                    <img className={classes['imgReadMore']} src={Nature} />
+                    <img className={classes.imgReadMore} src={Nature} />
                   </div>
                 </Col>
                 <Col lg={4}>
-                  <div className={classes['divImg']}>
-                    <img className={classes['img']} src={Warehouse} />
+                  <div className={classes.divImg}>
+                    <img className={classes.img} src={Warehouse} />
                   </div>
                 </Col>
                 <Col lg={4}>
-                  <div className={classes['divImg']}>
-                    <img className={classes['img']} src={Building} />
+                  <div className={classes.divImg}>
+                    <img className={classes.img} src={Building} />
                   </div>
                 </Col>
               </Row>
-              <div className="text-center">
+              <div className={classes.textCenter}>
                 <Link href={'/'}>
-                  <span className={classes['spanSeeMore']}>
-                    <a className={classes['seeMore']}>See more projects</a>
+                  <span className={classes.spanSeeMore}>
+                    <a className={classes.seeMore}>See more projects</a>
                   </span>
                 </Link>
               </div>
@@ -146,7 +207,7 @@ const Clean = () => {
           )}
         </div>
       </section>
-    </Fragment>
+    </div>
   );
 };
 
